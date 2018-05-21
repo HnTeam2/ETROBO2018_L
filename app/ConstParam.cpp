9c78fc0781b3ec318fc3ed958a7e66f17c670bb1
ShuzoTakahashi <shuzo.create@gmail.com>
2018-05-21 16:37:09 +0900

    ステージングし忘れ

--- app/ConstParam.cpp
+++ app/ConstParam.cpp
@@ -1,4 +1,16 @@
 #include "ConstParam.h"
 
-const float ConstParam::PID_TASK = {1.40,0.8,0.1};
-const float ConstParam::PID_TASK = 
+const float ConstParam::PID_TASK_1[3] = {1.40,0.8,0.1};
+const float ConstParam::PID_TASK_2[3] = {3.80, 0.8, 0.15};
+const float ConstParam::PID_TASK_3[3] = {1.0, 0.01, 0.1};
+const float ConstParam::PID_TASK_90[3] = {0.5, 0.01, 0.05};
+const float ConstParam::PID_TASK_LOW[3] ={1.0, 0.01, 0.1};
+const float ConstParam::PID_TASK_80[3] = {1.40, 0.8, 0.1};
+
+const int ConstParam::SPEED_TASK_1 = 80;
+const int ConstParam::SPEED_TASK_2 = 80;
+const int ConstParam::SPEED_TASK_3 = 35;
+const int ConstParam::SPEED_TASK_90 = 95;
+const int ConstParam::SPEED_TASK_LOW = 20;
+const int ConstParam::SPEED_TASK_80 = 80;
+
--- app/ConstParam.h
+++ app/ConstParam.h
@@ -1,6 +1,18 @@
 struct ConstParam
 {
-  static const int RETCODE_SUCCESS;
-  static const int RETCODE_FAIL;
-  static const int RETCODE_WARN;
+  static const float PID_TASK_1[3];
+  static const float PID_TASK_2[3];
+  static const float PID_TASK_3[3];
+  static const float PID_TASK_90[3];
+  static const float PID_TASK_LOW[3];
+  static const float PID_TASK_80[3];
+  static const float PID_TASK_OFF[3];
+
+  static const int SPEED_TASK_1;
+  static const int SPEED_TASK_2;
+  static const int SPEED_TASK_3;
+  static const int SPEED_TASK_90;
+  static const int SPEED_TASK_LOW;
+  static const int SPEED_TASK_80;
+
 };
